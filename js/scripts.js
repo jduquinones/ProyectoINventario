47fca1084083dabcd457f51cd68599027006f10d
jduquinones <eduardoqm23@hotmail.com>
2022-02-03 13:39:14 -0500

    login y principal__tabla en javascripts

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+
+ function cantidad(){
+    for (let i = 0; i < 10; i++) {
+      var fila = document.write("<tr></tr>")
+      document.getElementsByClassName("table__diseño").innerhtml = fila;      
+        
+    }
+}
+cantidad();
