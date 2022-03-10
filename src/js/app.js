68f5b25f413886fe5ee6ee77ec34aa61d1d28df5
Jose Trabajo <eduardoqm23@hotmail.com>
2022-03-10 01:21:38 -0500

    fin de overlay con java scripts

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     menuheader();
-    ampliarImagen();
+   
 });
 
 // Coloca una clase al dar clic en el menu, esto para hacer el efecto en el que aparace 'Registrar Equipo'
@@ -14,44 +14,31 @@ function menuheader(){
     }
 }
 
+// Funcion que me realiza un overlay
 function ampliarImagen(id){
 
     img = `${id}`;
-    var imagen = [document.querySelectorAll('.resultado-imagen')];
-    
-    imagen.onclick = trae(img);
+    const contenedor = document.querySelectorAll('.resultado-imagen');   
 
-    function trae(img) {
+    contenedor.onclick = trae()
+}  
 
-        imagen = document.images;
-        imagen.innerHTML = imagen.src;   
-        
-        for (let i = 0; i < imagen.length; i++) {
-
-            if (imagen.item(i).src.includes(img)) {
-                
-                const overlay = document.createElement('DIV');                
-                overlay.appendChild(imagen[i]);
-                overlay.classList.add('overlay');   
-                const body = document.querySelector('body');
-                body.appendChild(overlay);
-                
-
-                const x = document.createElement('P');
-                x.textContent = 'X';
-                x.classList.add('cerrar');
-                overlay.appendChild(x);
-                x.onclick = function() {
-                    x.classList.remove();
-                    x.remove();
-
-                    overlay.classList.remove();
-                    overlay.remove();
-                    
-                }
-                overlay.toggle();
-
-            }            
-        }        
+function trae(){
+    const div = document.createElement('DIV');    
+    const imagen = document.createElement('IMG');
+    imagen.setAttribute('src', img);
+    div.appendChild(imagen);   
+    div.classList.add('overlay'); 
+    // Boton para cerrar el Modal 
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function(){
+        div.remove();
     }
-}     
+    div.appendChild(cerrarModal);    
+
+    const body = document.querySelector('body');
+    body.appendChild(div);
+   
+}
